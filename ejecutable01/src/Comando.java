e11c154a9b0f25cdbe5fa5b1e387ca9b662a1fb3
Natalia <200351@iessierradeguara.com>
2021-10-18 11:10:32 +0200

    Añadido proyecto ejecutable01

---
+++ ejecutable01/src/Comando.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.concurrent.Executors;
+
+public class Comando {
+
+	public static void main(String[] args) throws IOException, InterruptedException{
+		// TODO Auto-generated method stub
+		System.out.println(System.getProperty("os.name"));
+		
+		Process proceso;
+		
+		proceso = Runtime.getRuntime().exec("ls / -l");
+
+		StreamGobbler streamGobbler = new StreamGobbler(proceso.getInputStream(), System.out::println);
+		Executors.newSingleThreadExecutor().submit(streamGobbler);
+		int exitCode = proceso.waitFor();
+		System.out.println("Código de finalización del proceso: " + exitCode);
+		
+	}
+
+}
